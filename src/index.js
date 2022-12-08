ae62434fb416968737c1d959b98c3f5a808953c5
Sammy-CK <sammycherono2696@gmail.com>
2022-12-08 18:52:15 +0300

    Initial completion

---
+++ src/index.js
@@ -0,0 +1,151 @@
+const ulForQuote = document.querySelector('#quote-list') //ul containing quotes
+
+const form = document.querySelector('#new-quote-form') //form for new quote
+
+const newQuote = document.querySelector('#new-quote') // new quote text submission
+
+const newAuthor = document.querySelector('#author') // new author text input
+
+
+//adding new quote
+form.addEventListener('submit', () => {
+// add it to json server
+    fetch('http://localhost:3000/quotes',{
+    method : "POST",
+    headers : {
+        "Content-Type" : "application/json",
+        Accept : "application/json"
+    },
+    body : JSON.stringify({
+        quote : newQuote.value,
+        author : newAuthor.value
+    })
+
+    })
+//end
+
+
+
+
+ //add the new quote on screen 
+    ulForQuote.innerHTML = ''
+
+//display all quotes including the added ones
+    showAllQuotes();
+
+
+})
+//end
+
+
+
+//fetching all quote data
+function showAllQuotes(){
+fetch('http://localhost:3000/quotes?_embed=likes')
+.then(resp => resp.json())
+.then(data => {console.log(data)
+// Individual quote data
+    data.forEach(overallQuote => {
+
+    
+ 
+//actual quote data to be on screen
+    const li = document.createElement('li')
+    li.classList.add('quote-card')
+    ulForQuote.appendChild(li)
+
+    const blockquote = document.createElement('blockquote')
+    blockquote.classList.add('blockquote')
+    li.appendChild(blockquote)
+
+    const p = document.createElement('p')
+    p.classList.add('mb-0');
+    p.innerText = overallQuote.quote
+    blockquote.appendChild(p)
+
+    const footer = document.createElement('footer')
+    footer.classList.add('blockquote-footer')
+    footer.innerText = overallQuote.author
+    blockquote.appendChild(footer)
+
+    const br = document.createElement('br')
+    blockquote.appendChild(br)
+
+
+
+    const buttonLike = document.createElement('button')
+    buttonLike.classList.add('btn-success')
+    buttonLike.innerText=`Likes:`
+
+//button creates like for this particular quote in the API and update the number of likes displayed    
+
+ buttonLike.addEventListener('click', () => {
+    fetch('http://localhost:3000/likes',{
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+            Accept : "application/json"
+        },
+        body : JSON.stringify({
+            quoteId: overallQuote.id,
+            createdAt : Date(),
+            likes : 0
+        })
+    })
+    .then(response => response.json())
+    .then(dataz => {
+        likeNoSpan.innerText = dataz.likes
+        console.log(dataz.likes)
+
+
+    })
+ })
+
+
+//end
+
+    blockquote.appendChild(buttonLike)
+
+
+
+
+
+    const likeNoSpan = document.createElement('span')
+
+    buttonLike.appendChild(likeNoSpan)
+
+    const buttonDelete = document.createElement('button')
+    buttonDelete.classList.add('btn-danger')
+    buttonDelete.innerText = `Delete`
+
+//delete button deletes the quote
+
+buttonDelete.addEventListener('click',() => {
+    fetch(`http://localhost:3000/quotes/${overallQuote.id}`,{
+        method : "DELETE",
+        headers : {
+            "Content-Type" : "application/json",
+            Accept : "application/json"
+        }
+    })
+ //delete the new quote on screen    
+ ulForQuote.innerHTML = ''
+
+ //display all quotes excluding the deleted ones
+     showAllQuotes();
+ 
+})
+
+
+//end
+
+    blockquote.appendChild(buttonDelete)
+
+
+})
+})
+}
+
+
+
+showAllQuotes();
